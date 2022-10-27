391b560eb093478b6776b1a151dba5604aa031dd
Arnold Andy Diaz Cabrera <74936852+Andy-12@users.noreply.github.com>
2022-10-27 11:17:19 -0400

    Initial commit

---
+++ Paddle.cpp
@@ -0,0 +1,60 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Paddle.h"
+#include "GameFramework/FloatingPawnMovement.h"
+#include "Components/StaticMeshComponent.h"
+#include "Ball.h"
+#include "BallBound.h"
+
+
+// Sets default values
+APaddle::APaddle()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	SM_Paddle = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("SM Paddle"));
+	RootComponent = SM_Paddle;
+
+	SM_Paddle->SetEnableGravity(false);
+	SM_Paddle->SetConstraintMode(EDOFMode::XZPlane);
+	SM_Paddle->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	SM_Paddle->SetCollisionProfileName(TEXT("PhisicActor"));
+
+	FloatingMovement = CreateDefaultSubobject<UFloatingPawnMovement>(TEXT("Floating Pawn Movement"));
+
+	aux = 0;
+}
+
+// Called when the game starts or when spawned
+void APaddle::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APaddle::Tick(float DeltaTime)
+{
+	
+	Super::Tick(DeltaTime);
+
+	/*if({
+		posicion.Insert(SM_Paddle->GetComponentLocation(), aux);
+		UE_LOG(LogTemp, Warning, TEXT("Se ah insertado satisfactoriamente"));
+		aux++;
+	}*/
+}
+
+// Called to bind functionality to input
+void APaddle::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+void APaddle::MoveHorizontal(float AxisValue)
+{
+	AddMovementInput(FVector(AxisValue, 0.0f, 0.0f), 1.0f, false);
+}
+
